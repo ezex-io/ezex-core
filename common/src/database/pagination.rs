af007e41b8d0d2fdc2d5e4ec27a27839f46a2498
sdrhashemi <sdrhashemi@gmail.com>
2025-05-05 13:04:19 +0330

    test(paginate): added unit tests for paginate

--- common/src/database/pagination.rs
+++ common/src/database/pagination.rs
@@ -1,9 +1,5 @@
 use diesel::{
-    pg::Pg,
-    prelude::*,
-    query_builder::*,
-    query_dsl::methods::LoadQuery,
-    sql_types::BigInt,
+    pg::Pg, prelude::*, query_builder::*, query_dsl::methods::LoadQuery, sql_types::BigInt,
 };
 
 // TODO Add test for me
@@ -72,5 +68,42 @@ where
         out.push_sql(" OFFSET ");
         out.push_bind_param::<BigInt, _>(&self.offset)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use diesel::pg::PgConnection;
+    use diesel::prelude::*;
+    use diesel::dsl::sql;
+
+    fn base_query() -> impl QueryFragment<Pg> + QueryId {
+        sql::<diesel::sql_types::Integer>("SELECT 1")
+    }
+
+    #[test]
+    fn test_paginate_defaults() {
+        let paged = base_query().paginate(1);
+        assert_eq!(paged.page, 1);
+        assert_eq!(paged.per_page, super::DEFAULT_PER_PAGE);
+        assert_eq!(paged.offset, 0);
+    }
+
+    #[test]
+    fn test_per_page_override() {
+        let paged = base_query().paginate(2).per_page(15);
+
+        assert_eq!(paged.per_page, 15);
+        assert_eq!(paged.offset, 15); // (2-1) * 15 = 15
+    }
+
+    #[test]
+    fn test_sql_generation() {
+        let paged = base_query().paginate(3).per_page(20);
+        let generated_sql = debug_query::<Pg, _>(&paged).to_string();
+
+        assert!(generated_sql.contains("LIMIT $1"));
+        assert!(generated_sql.contains("OFFSET $2"));
     }
 }
